d4ee20594a987faf2360385839c66bd8ff2ed280
Johan Fabi <johanfabi.nttdata@latam.com>
2024-05-21 17:51:15 -0400

    Add (main.py): New Python code on jenkins-docker

--- jenkins-docker/src/main.py
+++ jenkins-docker/src/main.py
@@ -1,7 +1,22 @@
-import os
+"""
+This module fetches data from the PokeAPI and prints information about a specific Pokemon.
+"""
 
-def jenkins_build():
-    print(f"Hello from Python - Jenkins Build {os.environ['BUILD_NUMBER']}")
-    
-jenkins_build()
-    
+import requests
+
+
+URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    data = response.json()
+    name = data["name"]
+    species = data["species"]["name"]
+    types = [t["type"]["name"] for t in data["types"]]
+
+    print(f"Nombre: {name}")
+    print(f"Especie: {species}")
+    print(f"Tipos: {', '.join(types)}")
+else:
+    print("Error al obtener los datos de la API")
