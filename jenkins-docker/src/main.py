4520fe7677125099ef49e8f1f1f6678744043b2d
Johan Fabi <johanfabi.nttdata@latam.com>
2024-05-20 23:10:19 -0400

    Edit (main.py): Print Jenkins Build

--- jenkins-docker/src/main.py
+++ jenkins-docker/src/main.py
@@ -1,7 +1,7 @@
 import os
 
 def jenkins_build():
-    print(f"Hello from {os.environ['BUILD_NUMBER']}")
+    print(f"Hello from Python - Jenkins Build {os.environ['BUILD_NUMBER']}")
     
 jenkins_build()
     
