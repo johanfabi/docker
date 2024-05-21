5929b477cd7d6570017372b09292f9ea73f022b3
Johan Fabi <johanfabi.nttdata@latam.com>
2024-05-21 18:04:00 -0400

    Update (jenkins-docker): Add environment variable on main.py.

--- jenkins-docker/src/main.py
+++ jenkins-docker/src/main.py
@@ -3,20 +3,20 @@ This module fetches data from the PokeAPI and prints information about a specifi
 """
 
 import requests
+import os
 
+POKEMON = os.environ.get("POKEMON")
 
-URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
+URL = f"https://pokeapi.co/api/v2/pokemon/{POKEMON}"
 
 response = requests.get(URL)
 
 if response.status_code == 200:
     data = response.json()
     name = data["name"]
-    species = data["species"]["name"]
     types = [t["type"]["name"] for t in data["types"]]
 
     print(f"Nombre: {name}")
-    print(f"Especie: {species}")
     print(f"Tipos: {', '.join(types)}")
 else:
     print("Error al obtener los datos de la API")
